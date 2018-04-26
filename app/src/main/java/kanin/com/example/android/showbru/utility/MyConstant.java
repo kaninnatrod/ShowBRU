c55186bf7612a9c75ea971fe4404f55af5fa92b4
kanin <krukaninnatrod@gmail.com>
2018-04-26 16:33:16 +0700

    Brake 2 day2

--- app/src/main/java/kanin/com/example/android/showbru/utility/MyConstant.java
+++ app/src/main/java/kanin/com/example/android/showbru/utility/MyConstant.java
@@ -3,6 +3,11 @@ package kanin.com.example.android.showbru.utility;
 public class MyConstant {
 
     private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
+    private String urlGetAllUser ="http://androidthai.in.th/bru/getAllUser.php";
+
+    public String getUrlGetAllUser() {
+        return urlGetAllUser;
+    }
 
     public String getUrlAddUser() {
         return urlAddUser;
